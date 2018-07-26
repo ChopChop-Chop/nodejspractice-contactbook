5e8c1b05ba87dee91a3bf85a69eab5961c36886b
EmptyBrain <newell729@naver.com>
2018-07-26 12:45:48 +0900

    complete basic feature

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var mongoose = require("mongoose");
 var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
 var app = express();
 
 // DB Setting ...
@@ -14,7 +15,6 @@ db.on("error", function(err){
 });
 
 // Other Setting
-
 app.set("view engine", "ejs");
 app.use(express.static(__dirname+"/public"));
 app.use(bodyParser.json()); // 2
@@ -22,6 +22,12 @@ app.use(bodyParser.urlencoded({extended:true}));  // 3
 /* body Parser를 이용해 스트림의 form data를 req.body로 옮겨 담습니다.
 2번은 json 데이터를 3번은 urlencoded data를 분석해서 req.body를 생성합니다.
 app.use()의 첫번째 인자가 비어있으면 웹앱이 요청을 수신할 때마다 인자의 함수가 실행됩니다.
+*/
+app.use(methodOverride("_method"));
+/*
+_method의 query로 들어오는 값으로 HTTP method를 바꿉니다. 
+예를들어 http://example.com/category/id?_method=delete를 받으면 
+_method의 값인 delete을 읽어 해당 request의 HTTP method를 delete으로 바꿉니다.
 */
 
 // DB schema // 4
@@ -56,5 +62,34 @@ app.post("/contacts", function(req, res){
         res.redirect("/contacts");
     });
 });
+// Contacts - Show
+app.get("/contacts/:id", function(req, res) {
+    Contact.findOne({_id:req.params.id}, function(err, contact){
+        if(err) return res.json(err);
+        res.render("contacts/show", {contact:contact});
+    });
+});
+// Contacts - Edit
+app.get("/contacts/:id/edit", function(req, res) {
+    Contact.findOne({_id:req.params.id}, function(err, contact){
+        if(err) return res.json(err);
+        res.render("contacts/edit", {contact:contact});
+    });
+});
+// Contacts - Update
+app.put("/contacts/:id", function(req, res) {
+    Contact.findOneAndUpdate({_id:req.params.id}, req.body, function(err, contact){
+        if(err) return res.json(err);
+        res.redirect("/contacts/" + req.params.id);
+    });
+});
+// Contacts - Destroy
+app.delete("/contacts/:id", function(req, res) {
+    Contact.remove({_id:req.params.id}, function(err, contact){
+        if(err) return res.json(err);
+        res.redirect("/contacts");
+    });
+});
+
 
 app.listen(3000);
